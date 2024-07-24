94c5210140a5244591e88989c3454e82416843b5
Karthi Srinivasan <87223671+karthisrinivasan@users.noreply.github.com>
2024-07-23 23:28:00 -0400

    add hover definitions provider

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,7 +1,7 @@
 
 import { File } from 'buffer';
 import * as path from 'path';
-import { workspace, ExtensionContext, languages, DefinitionProvider, TextDocument, Position, CancellationToken, Location, Uri } from 'vscode';
+import { workspace, ExtensionContext, languages, DefinitionProvider, TextDocument, Position, CancellationToken, Location, Uri, Hover } from 'vscode';
 
 import {
 	LanguageClient,
@@ -54,6 +54,192 @@ export function activate(context: ExtensionContext) {
 		}
 	};
 
+	context.subscriptions.push(
+		languages.registerHoverProvider('act', {
+			provideHover(document, position, token) {
+
+				const range = document.getWordRangeAtPosition(position);
+				const word = document.getText(range);
+
+				// console.log(`Word cursor hovered: ${word}`);
+
+				// Create the hover object
+				var hover;
+				if (word === "import") {
+					hover = new Hover("Import a namespace or file");
+				} 
+				else if (word === "open") {
+					hover = new Hover("Open a namespace");
+				}
+				else if (word === "template") {
+					hover = new Hover("Define a type template");
+				}
+				else if (word === "export") {
+					hover = new Hover("Export a namespace or user-defined type");
+				}
+				else if (word === "namespace") {
+					hover = new Hover("Define a namespace");
+				}
+				else if (word === "defproc") {
+					hover = new Hover("Define a process");
+				}
+				else if (word === "defcell") {
+					hover = new Hover("Define a cell");
+				}
+				else if (word === "defchan") {
+					hover = new Hover("Define a channel");
+				}
+				else if (word === "deftype") {
+					hover = new Hover("Define a new type");
+				}
+				else if (word === "function") {
+					hover = new Hover("Define a function");
+				}
+				else if (word === "log") {
+					hover = new Hover("Built-in chp log function");
+				}
+				else if (word === "self") {
+					hover = new Hover("output variable name for functions");
+				}
+				else if (word === "methods") {
+					hover = new Hover("Methods body for deftype objects");
+				}
+				else if (word === "set") {
+					hover = new Hover("Method for writing a value to the type");
+				}
+				else if (word === "get") {
+					hover = new Hover("Method for reading the value of the type");
+				}
+				else if (word === "send_up") {
+					hover = new Hover("First half of a send synchronization");
+				}
+				else if (word === "recv_up") {
+					hover = new Hover("First half of a receive synchronization");
+				}
+				else if (word === "send_rest") {
+					hover = new Hover("Complete the rest of the send protocol");
+				}
+				else if (word === "recv_rest") {
+					hover = new Hover("Complete the rest of the receive protocol");
+				}
+				else if (word === "send_probe") {
+					hover = new Hover("Probe operation for sending end");
+				}
+				else if (word === "recv_probe") {
+					hover = new Hover("Probe operation for receiving end");
+				}
+				else if (word === "interface") {
+					hover = new Hover("Define an interface for a process");
+				}
+				else if (word === "skip") {
+					hover = new Hover("Empty chp action that does nothing");
+				}
+				else if (word === "else") {
+					hover = new Hover("Special catch-all guard for selections");
+				}
+				else if (word === "bool") {
+					hover = new Hover("Pre-defined boolean data type");
+				}
+				else if (word === "int") {
+					hover = new Hover("Pre-defined integer data type");
+				}
+				else if (word === "pint") {
+					hover = new Hover("Parametric integer type");
+				}
+				else if (word === "preal") {
+					hover = new Hover("Parametric real-valued type");
+				}
+				else if (word === "pbool") {
+					hover = new Hover("Parametric boolean type");
+				}
+				else if (word === "ptype") {
+					hover = new Hover("Special meta-parameter type");
+				}
+				else if (word === "chan") {
+					hover = new Hover("Pre-defined abstract channel type");
+				}
+				else if (word === "enum") {
+					hover = new Hover("Pre-defined enumeration type");
+				}
+				else if (word === "chp") {
+					hover = new Hover("Define a CHP sublanguage body");
+				}
+				else if (word === "prs") {
+					hover = new Hover("Define a PRS sublanguage body");
+				}
+				else if (word === "hse") {
+					hover = new Hover("Define a HSE sublanguage body");
+				}
+				else if (word === "dataflow") {
+					hover = new Hover("Define a Dataflow sublanguage body");
+				}
+				else if (word === "dataflow_cluster") {
+					hover = new Hover("Group dataflow elements together");
+				}
+				else if (word === "order") {
+					hover = new Hover("Define a computation order for a dataflow body");
+				}
+				else if (word === "refine") {
+					hover = new Hover("Define a refinement body");
+				}
+				else if (word === "sizing") {
+					hover = new Hover("Define a sizing body");
+				}
+				else if (word === "spec") {
+					hover = new Hover("Define a specification body");
+				}
+				else if (word === "exclhi") {
+					hover = new Hover("Assert that two signals are exclusive-high");
+				}
+				else if (word === "excllo") {
+					hover = new Hover("Assert that two signals are exclusive-low");
+				}
+				else if (word === "mk_exclhi") {
+					hover = new Hover("Force two signals to be exclusive-high in simulation");
+				}
+				else if (word === "mk_excllo") {
+					hover = new Hover("Force two signals to be exclusive-low in simulation");
+				}
+				else if (word === "timing") {
+					hover = new Hover("Define a timing body");
+				}
+				else if (word === "Vdd") {
+					hover = new Hover("Global Vdd signal");
+				}
+				else if (word === "GND") {
+					hover = new Hover("Global ground signal");
+				}
+				else if (word === "passp") {
+					hover = new Hover("PMOS pass transistor (G,S,D)");
+				}
+				else if (word === "passn") {
+					hover = new Hover("NMOS pass transistor (G,S,D)");
+				}
+				else if (word === "keeper") {
+					hover = new Hover("Keeper type for production rule [0/1/2/3]");
+				}
+				else if (word === "cap") {
+					hover = new Hover("Capacitor");
+				}
+				else if (word === "macro") {
+					hover = new Hover("Define a macro");
+				}
+				else if (word === "defenum") {
+					hover = new Hover("Define an enumeration");
+				}
+				else if (word === "chp-txt") {
+					hover = new Hover("Define a CHP-TXT sublanguage body");
+				}
+				else if (word === "transgate") {
+					hover = new Hover("Transmission gate (Gn,Gp,S,D)");
+				}
+				else {
+					hover = undefined;
+				}
+				return hover;
+			}
+		}
+	))
 	// context.subscriptions.push(languages.registerDefinitionProvider(
 	// 		[{ scheme: 'file', language: 'plaintext' }], new ACTDefinitionProvider()))
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -154,7 +154,8 @@ connection.languages.diagnostics.on(async (params) => {
 	if (document !== undefined) {
 		return {
 			kind: DocumentDiagnosticReportKind.Full,
-			items: await validateTextDocument(document)
+			// items: await validateTextDocument(document)
+			items: []
 		} satisfies DocumentDiagnosticReport;
 	} else {
 		// We don't know the document. We can either try to read it from disk
@@ -168,55 +169,55 @@ connection.languages.diagnostics.on(async (params) => {
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
-documents.onDidChangeContent(change => {
-	validateTextDocument(change.document);
-	// refManager.updateDocumentReferences(change.document.uri);
-});
+// documents.onDidChangeContent(change => {
+// 	validateTextDocument(change.document);
+// 	// refManager.updateDocumentReferences(change.document.uri);
+// });
 
-async function validateTextDocument(textDocument: TextDocument): Promise<Diagnostic[]> {
-	// In this simple example we get the settings for every validate run.
-	const settings = await getDocumentSettings(textDocument.uri);
+// async function validateTextDocument(textDocument: TextDocument): Promise<Diagnostic[]> {
+// 	// In this simple example we get the settings for every validate run.
+// 	const settings = await getDocumentSettings(textDocument.uri);
 
-	// The validator creates diagnostics for all uppercase words length 2 and more
-	const text = textDocument.getText();
-	const pattern = /\b[A-Z]{2,}\b/g;
-	let m: RegExpExecArray | null;
+// 	// The validator creates diagnostics for all uppercase words length 2 and more
+// 	const text = textDocument.getText();
+// 	const pattern = /\b[A-Z]{2,}\b/g;
+// 	let m: RegExpExecArray | null;
 
-	let problems = 0;
-	const diagnostics: Diagnostic[] = [];
-	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-		problems++;
-		const diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
-			range: {
-				start: textDocument.positionAt(m.index),
-				end: textDocument.positionAt(m.index + m[0].length)
-			},
-			message: `${m[0]} is all uppercase.`,
-			source: 'ex'
-		};
-		if (hasDiagnosticRelatedInformationCapability) {
-			diagnostic.relatedInformation = [
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Spelling matters'
-				},
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Particularly for names'
-				}
-			];
-		}
-		diagnostics.push(diagnostic);
-	}
-	return diagnostics;
-}
+// 	let problems = 0;
+// 	const diagnostics: Diagnostic[] = [];
+// 	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
+// 		problems++;
+// 		const diagnostic: Diagnostic = {
+// 			severity: DiagnosticSeverity.Warning,
+// 			range: {
+// 				start: textDocument.positionAt(m.index),
+// 				end: textDocument.positionAt(m.index + m[0].length)
+// 			},
+// 			message: `${m[0]} is all uppercase.`,
+// 			source: 'ex'
+// 		};
+// 		if (hasDiagnosticRelatedInformationCapability) {
+// 			diagnostic.relatedInformation = [
+// 				{
+// 					location: {
+// 						uri: textDocument.uri,
+// 						range: Object.assign({}, diagnostic.range)
+// 					},
+// 					message: 'Spelling matters'
+// 				},
+// 				{
+// 					location: {
+// 						uri: textDocument.uri,
+// 						range: Object.assign({}, diagnostic.range)
+// 					},
+// 					message: 'Particularly for names'
+// 				}
+// 			];
+// 		}
+// 		diagnostics.push(diagnostic);
+// 	}
+// 	return diagnostics;
+// }
 
 connection.onDidChangeWatchedFiles(_change => {
 	// Monitored files have change in VSCode
@@ -623,7 +624,7 @@ connection.onCompletionResolve(
 		}
 		else if (item.data === 13) {
 			item.detail = 'Methods body for deftype objects';
-			item.documentation = 'blah blah';
+			item.documentation = '';
 		}
 		else if (item.data === 14) {
 			item.detail = 'Method for writing a value to the type';
